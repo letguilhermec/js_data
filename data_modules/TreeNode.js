3a1f69278fd28e03d2e898642bbd0ab1cf19b5ab
Guilherme Correa <correacgui@gmail.com>
2022-07-20 18:54:29 -0300

    Added TreeNode.js

---
+++ data_modules/TreeNode.js
@@ -0,0 +1,30 @@
+class TreeNode {
+  constructor(data) {
+    this.data = data
+    this.children = []
+  }
+
+  addChild(child) {
+    if (child instanceof TreeNode) {
+      this.children.push(child)
+    } else {
+      this.children.push(new TreeNode(child))
+    }
+  }
+
+  removeChild(childToRemove) {
+    const length = this.children.length
+    this.children = this.children.filter(child => {
+      if (childToRemove instanceof TreeNode) {
+        return childToRemove !== child
+      } else {
+        return childToRemove !== child.data
+      }
+    })
+    if (length === this.children.length) {
+      this.children.forEach(child => child.removeChild(childToRemove))
+    }
+  }
+}
+
+module.exports = TreeNode
